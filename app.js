b29a19224460e207e0625e7dc799fb38e9236938
“postlude <useseungdeokhan@naver.com>
2020-07-27 23:02:38 +0900

    add morgan

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 require('module-alias/register');
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = 3000;
+const logFrmt = ':remote-addr - [:method] :url :status :response-time ms';
 
+app.use(morgan(logFrmt));
 app.use('/test', require('@/routes/test_router'));
 
 const server = app.listen(port, () => {
     console.log('==================== [MyCmd WAS] ====================');
-    console.log(`Server listening on port ${port}`);
+    console.log(`- Port : ${port}`);
+    console.log('=====================================================');
 });
