aa76d89f81f90f2fd32ad88d9ae7e9605f40666c
postlude <seungdeokhan@naver.com>
2022-04-12 00:35:32 +0900

    add entity, modify unused column

--- src/database/entity/execution-statement-tag.entity.ts
+++ src/database/entity/execution-statement-tag.entity.ts
@@ -1,12 +1,12 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity({ database: 'postlude', name: 'dev_link_tag' })
+@Entity({ database: 'postlude', name: 'execution_statement_tag' })
 export class DevLinkTag {
 	@PrimaryGeneratedColumn({ type: 'int', name: 'idx', unsigned: true })
 	idx: number;
 
-	@Column({ type: 'int', name: 'dev_link_idx', unsigned: true })
-	devLinkIdx: number;
+	@Column({ type: 'int', name: 'execution_statement_idx', unsigned: true })
+	executionStatementIdx: number;
 
 	@Column({ type: 'int', name: 'tag_idx', unsigned: true })
 	tagIdx: number;
