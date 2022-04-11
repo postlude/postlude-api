aea404c843fd57515522f7805398670343046494
postlude <seungdeokhan@naver.com>
2022-04-11 22:30:53 +0900

    move /entity to /database/entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DevLinkModule } from './module/dev-link/dev-link.module';
-import { DevLink } from './entity/dev-link.entity';
-import { Tag } from './entity/tag.entity';
-import { DevLinkTag } from './entity/dev-link-tag.entity';
+import { DevLink } from './database/entity/dev-link.entity';
+import { Tag } from './database/entity/tag.entity';
+import { DevLinkTag } from './database/entity/dev-link-tag.entity';
 import { APP_PIPE } from '@nestjs/core';
 
 @Module({
