530f83653ebf10e24ad5570a5577157d7d5f48d7
postlude <seungdeokhan@naver.com>
2023-06-07 22:22:44 +0900

    modify column name

--- src/database/entity/tag.entity.ts
+++ src/database/entity/tag.entity.ts
@@ -5,8 +5,8 @@ export class Tag {
 	@PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
 	id: number;
 
-	@Column({ type: 'varchar', name: 'tag', unique: true })
-	tag: string;
+	@Column({ type: 'varchar', name: 'name', unique: true })
+	name: string;
 
 	@Column({ type: 'datetime', name: 'created_at' })
 	createdAt: Date;
