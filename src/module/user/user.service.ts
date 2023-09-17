bff64629a26df59f75a64ca9ebd9a8eef86e5c0b
postlude <seungdeokhan@naver.com>
2023-09-17 23:53:25 +0900

    change to auth api

--- src/module/user/user.service.ts
+++ src/module/user/user.service.ts
@@ -1,13 +1,11 @@
+import { ConflictException, Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { UserRepository } from 'src/database/repository/user.repository';
 import { UserDto } from './user.dto';
 
 @Injectable()
 export class UserService {
 	constructor(
-		private readonly jwtService: JwtService,
 		private readonly userRepository: UserRepository
 	) {}
 
@@ -33,26 +31,5 @@ export class UserService {
 		});
 
 		return identifiers[0].id as number;
-	}
-
-	/**
-	 * @description 로그인
-	 * @param param
-	 */
-	public async signIn(param: UserDto) {
-		const { email, password: inputPassword } = param;
-
-		const user = await this.userRepository.findOne({ email });
-
-		if (user) {
-			const { id, email, password } = user;
-
-			const isSame = await bcrypt.compare(inputPassword, password);
-			if (isSame) {
-				return this.jwtService.sign({ id, email });
-			}
-		}
-
-		throw new UnauthorizedException();
 	}
 }
