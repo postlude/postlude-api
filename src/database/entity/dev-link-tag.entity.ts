9bfac8071872f23121c112e2874d6d121b90c9a2
postlude <seungdeokhan@naver.com>
2024-02-22 00:48:06 +0900

    modify entity

--- src/database/entity/dev-link-tag.entity.ts
+++ src/database/entity/dev-link-tag.entity.ts
@@ -1,21 +1,10 @@
-import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { DevLink } from './dev-link.entity';
+import { Entity, PrimaryColumn } from 'typeorm';
 
 @Entity({ database: 'postlude', name: 'dev_link_tag' })
 export class DevLinkTag {
-	@PrimaryGeneratedColumn({ type: 'int', name: 'id', unsigned: true })
-	id: number;
-
-	@Column({ type: 'int', name: 'dev_link_id', unsigned: true })
+	@PrimaryColumn({ type: 'int', name: 'dev_link_id', unsigned: true })
 	devLinkId: number;
 
-	@Column({ type: 'int', name: 'tag', unsigned: true })
-	tag: string;
-
-	@Column({ type: 'datetime', name: 'created_at' })
-	createdAt: Date;
-
-	@ManyToOne(() => DevLink, (devLink) => devLink.devLinkTags, { onDelete: 'CASCADE' })
-	@JoinColumn({ name: 'dev_link_id' })
-	devLink: DevLink;
+	@PrimaryColumn({ type: 'int', name: 'tag_id', unsigned: true })
+	tagId: number;
 }
